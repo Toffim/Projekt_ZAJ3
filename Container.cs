53d50e5ec4f0650652156cdcb21df1b47f1b27b4
Toffim <98185616+Toffim@users.noreply.github.com>
2025-03-18 14:33:27 +0100

    Add Cargo Container base class

---
+++ Container.cs
@@ -0,0 +1,54 @@
+﻿namespace Projekt_ZAJ3;
+
+public class Container
+{
+    private Container(int cargoMass, int massContainer, int height, int depth, int maximumCapacity)
+    {
+        this.cargoMass = cargoMass;
+        this.massContainer = massContainer;
+        this.height = height;
+        this.depth = depth;
+        this.maximumCapacity = maximumCapacity;
+
+        GenerateSerialNumber();
+    }
+
+    protected virtual void GenerateSerialNumber()
+    {
+        serialNumber = "KON-" + "B-" + lastCargoIndex.ToString();
+        lastCargoIndex++;
+    }
+
+    public void UnloadCargo()
+    {
+        SetCargoMass(0);
+    }
+
+    public void LoadCargo(int cargoMass)
+    {
+        cargoMass = cargoMass;
+    }
+
+    public void SetCargoMass(int cargoMass)
+    {
+        if (cargoMass > maximumCapacity)
+        {
+            throw new OverfillException("Maximum capacity of cargo exceeded.");
+        }
+        cargoMass = cargoMass;
+    }
+
+    public int GetCargoMass()
+    {
+        return cargoMass;
+    }
+// Vars
+    private static int lastCargoIndex = 1;
+    private string serialNumber;
+
+    public int cargoMass;
+    private int massContainer;
+    private int height;
+    private int depth;
+    private int maximumCapacity;
+}
---
+++ OverfillException.cs
@@ -0,0 +1,16 @@
+﻿namespace Projekt_ZAJ3;
+
+using System;
+
+public class OverfillException : Exception
+{
+    public OverfillException()
+    {
+    }
+    
+    public OverfillException(string message)
+        : base(message)
+    {
+        
+    }
+}
