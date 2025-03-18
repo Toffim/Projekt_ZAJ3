855a7f4919e9630d9ebac5d244527a7db489a2a8
Toffim <98185616+Toffim@users.noreply.github.com>
2025-03-18 15:13:00 +0100

    Add ContainerGas

---
+++ ContainerGas.cs
@@ -0,0 +1,40 @@
+﻿namespace Projekt_ZAJ3;
+
+public class ContainerGas : Container, IHazardNotifier
+{
+    public ContainerGas(double airPressure, double cargoMass, double containerMass, int height, int depth, double maximumCapacity) : base(cargoMass, containerMass, height, depth, maximumCapacity)
+    {
+        airPressure = airPressure;
+    }
+
+    protected override void GenerateSerialNumber()
+    {
+        serialNumber = "KON-" + "G-" + lastCargoIndex.ToString();
+        Container.lastCargoIndex++;
+    }
+
+    public override void UnloadCargo()
+    {
+        cargoMass = (0.05 * cargoMass);
+    }
+
+    public override void LoadCargo(double massToLoad)
+    {
+        base.LoadCargo(massToLoad);
+    }
+
+    public double GetAirPressure()
+    {
+        return airPressure;
+    }
+
+    public void Notify(string serialNumber)
+    {
+        {
+            Console.WriteLine($"Critical Situation with Gas Cargo [{serialNumber}]");
+        }
+    }
+
+    // Vars
+    private double airPressure;
+}
